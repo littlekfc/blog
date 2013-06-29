c1702563444c1b2a49e4d3c3e4b24a1be6f4d522
littlekfc <littlekfc@littlekfc-GA-MA78LM-S2.(none)>
2013-06-29 23:21:07 +0800

    add server

--- server/http-client.js
+++ server/http-client.js
@@ -2,6 +2,8 @@ var jsdom = require('jsdom');
 
 jsdom.env('http://www.zhihu.com/people/rao-ji', ['http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'], function(errors, window){
         var a = window.$(".zm-profile-section-item.zm-item.clearfix");
+        var self = window.$(".zm-profile-header-img zg-avatar-big zm-avatar-editor-preview").attr("src")
+        console.log(self);
         //console.log(window.$("#zh-profile-activity-page-list").html());
     });
 
