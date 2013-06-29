6fdb8f0ed404fedc1d38294ee23141cb5168efd2
littlekfc <littlekfc@littlekfc-GA-MA78LM-S2.(none)>
2013-06-29 23:32:50 +0800

    server update

--- server/http-client.js
+++ server/http-client.js
@@ -1,9 +1,14 @@
 var jsdom = require('jsdom');
-
-jsdom.env('http://www.zhihu.com/people/rao-ji', ['http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js'], function(errors, window){
+var fs = require("fs");
+var jquery = fs.readFileSync("./jquery.js").toString();
+jsdom.env({
+          html: 'http://www.zhihu.com/people/rao-ji',
+          src: [jquery],
+          done : function(errors, window){
         var a = window.$(".zm-profile-section-item.zm-item.clearfix");
-        var self = window.$(".zm-profile-header-img zg-avatar-big zm-avatar-editor-preview").attr("src")
+        var self = window.$(".zm-profile-header-img.zg-avatar-big.zm-avatar-editor-preview").attr("src");
         console.log(self);
         //console.log(window.$("#zh-profile-activity-page-list").html());
+        }
     });
 
